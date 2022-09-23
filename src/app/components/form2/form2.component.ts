84e2446cf60b02a2d39406d2fcf0447850231769
tanersaydam <tanersaydam@gmail.com>
2022-09-23 15:02:23 +0300

    .

--- src/app/components/form2/form2.component.ts
+++ src/app/components/form2/form2.component.ts
@@ -1,6 +1,7 @@
 import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { SwalService } from 'src/app/services/swal.service';
 import { PersonelModel } from '../form1/models/form1.model';
 
 @Component({
@@ -25,13 +26,16 @@ export class Form2Component implements OnInit {
   ]
 
   kayitForm: FormGroup;
+  updateForm: FormGroup;
 
   constructor(
-    private _date: DatePipe
+    private _date: DatePipe,
+    private _swal: SwalService
   ) { }
 
   ngOnInit(): void {
     this.createKayitForm();
+    
   }
 
   createKayitForm(){
@@ -44,12 +48,26 @@ export class Form2Component implements OnInit {
     })
   }
 
-  getir(index: number){
+  createUpdateForm(){
+    this.updateForm = new FormGroup({
+      personelAdi: new FormControl(this.updateModel.personelAdi,[Validators.required, Validators.minLength(3)]),
+      bolumu: new FormControl(this.updateModel.bolumu, Validators.required),
+      maasi: new FormControl(this.updateModel.maasi, [Validators.required, Validators.min(5500)]),
+      iseGirisTarihi: new FormControl(this._date.transform(this.updateModel.iseGirisTarihi, 'yyyy-MM-dd'), Validators.required),
+      tcNo: new FormControl(this.updateModel.tcNo,[Validators.required, Validators.minLength(11),Validators.maxLength(11), Validators.pattern(/^-?(0|[1-9]\d*)?$/)]),
+      id: new FormControl(this.updateModel.id, Validators.required)
+    })
+  }
 
+  getir(index: number){
+    this.isUpdateFormActive = true;
+    this.updateModel = this.personels[index]
+    this.createUpdateForm();
   }
 
   delete(index: number){
-
+    this.personels.splice(index,1);
+    this._swal.callSwal("Personel kaydı başarıyla silindi","Başarılı","info");
   }
 
   isSelectedItem(event: any){
@@ -57,7 +75,15 @@ export class Form2Component implements OnInit {
   }
 
   kaydet(){
+    //console.log(this.kayitForm.value);   
+    if (!this.kayitForm.valid) {
+      this._swal.callSwal("Zorunlu alanları doldurun!","Validasyon Hatası!","error")
+      return;
+    } 
 
+    this.personels.push(this.kayitForm.value);
+    this._swal.callSwal("Personel kayıt işlemi başarıyla tamamlandı","Başarılı","success")
+    this.createKayitForm();
   }
 
   guncelle(){
@@ -66,5 +92,18 @@ export class Form2Component implements OnInit {
 
   vazgec(){
 
+  }
+
+  checkInputForValid(name: string){
+    //console.log(this.kayitForm.controls[name]);
+    if (!this.kayitForm.controls[name].touched) {
+      return "form-control"
+    }
+
+    if(this.kayitForm.controls[name].valid){
+      return "form-control is-valid"
+    }
+
+    return "form-control is-invalid"
   }
 }
